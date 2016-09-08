e8e2637e9ca1fc7524f0b4059496fc10891bd7c8
tavisca-mbhanushali <mbhanushali@tavisca.com>
2016-09-08 18:47:44 +0530

    Employee Service with Console Application and Serialization Done

---
+++ Client/Client/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Client;
+using Client.EmployeeService;
+using System.Runtime.Serialization;
+
+namespace ClientWCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeServiceClient client = new EmployeeServiceClient();
+            var x = client.GetEmployee(1);
+
+            Console.WriteLine(x.Id);
+            Console.WriteLine(x.Name);
+            Console.WriteLine(x.DateOfBirth);
+            Console.WriteLine(x.Gender);
+
+            Console.ReadKey();
+
+            Employee emp = new Employee() { Id = 4, Name = "Nikhil", DateOfBirth = DateTime.Now, Gender = "Male" };
+            client.SaveEmployee(emp);
+        }
+
+
+            public static class Serializer
+        {
+            public static string Serialize(Object ob)
+            {
+                using (System.IO.MemoryStream memory = new System.IO.MemoryStream())
+                {
+                    DataContractSerializer serializer = new DataContractSerializer(ob.GetType());
+                    {
+                        serializer.WriteObject(memory, ob);
+                        return Encoding.UTF8.GetString(memory.ToArray());
+                    }
+
+                }
+
+            }
+        }
+
+
+        public static class Deserializer
+        {
+            public static T Deserialize<T>(string data)
+            {
+                DataContractSerializer deserializer = new DataContractSerializer(typeof(T));
+
+                return (T)deserializer.ReadObject(System.Xml.XmlReader.Create(new System.IO.StringReader(data)));
+            }
+        }
+
+    }
+
+  }
+
---
+++ EmployeeService/EmployeeService/EmployeeService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace EmployeeService
+{
+   
+    public class EmployeeService : IEmployeeService
+    {
+        static List<Employee> Employeelist;
+       public EmployeeService()
+        {
+            if (Employeelist == null)
+            {
+                Employeelist = new List<Employee>();
+                Employeelist.Add(new Employee { Id = 1, Name = "Mahesh", Gender = "Male", DateOfBirth = DateTime.Now });
+                Employeelist.Add(new Employee { Id = 2, Name = "Jaya", Gender = "Female", DateOfBirth = DateTime.Now });
+            }
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return Employeelist.Find(x => x.Id.Equals(id));
+        }
+
+        public void SaveEmployee(Employee Employee)
+        {
+            Employeelist.Add(Employee);
+            
+        }
+    }
+}
